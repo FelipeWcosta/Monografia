8bf88e35a4a728cfd32ef045f7f93fb926c0764f
FelipeWcosta <83873110+FelipeWcosta@users.noreply.github.com>
2023-06-07 21:36:57 -0300

    Update Neuronio-Artificial.py

--- Códigos/Neuronio-Artificial.py
+++ Códigos/Neuronio-Artificial.py
@@ -1,49 +1,41 @@
 import numpy as np
-class perceptron(object):
-    """
-        eta: Coeficiente de treinamento
-        i: Número de iterações
-        n_Xj: Número de entradas
 
-        Wj: Vetor de pesos iniciado com zeros
-    """
-    def __init__(self, n_Xj, eta = 0.01, i = 100):
-        self.eta = eta
-        self.i = i
-        self.Wj = np.zeros(n_Xj + 1)
-
-    def predict(self, inputs):
-        z = np.dot(inputs, self.Wj[1:]) + self.Wj[0]
-        if z >= 0:
-            sigma = 1
-        else:
-            sigma = 0
-        return sigma
+class Perceptron(object):
+  
+  def __init__(self, n_Xj, i=100, eta = 0.01):
+    self.i = i
+    self.eta = eta
+    self.Wj = np.zeros(n_Xj +1)
     
-    """
-        Xj: Entradas de treinamento
-        y: Saídas esperadas
-    """
-
-    def fit(self, Xj, y):
-        for _ in range(self.i):
-            for inputs, labels in zip(Xj, y):
-                predicao = self.predict(inputs)
-                self.Wj += self.eta*(labels - predicao)*inputs
-                self.Wj += self.eta*(labels - predicao)
+  def predict(self, inputs):
+    z = np.dot(self.Wj[1:], inputs) + self.Wj[0]
+    if z >= 0:
+      return 1
+    else:
+      return 0
+  
+  def train(self, Xj, y):
+    for _ in range(self.i):
+      for inputs,label in zip(Xj, y):
+        prediction = self.predict(inputs)
+        self.Wj[1:] += self.eta*(label - prediction)*inputs
+        self.Wj[0] += self.eta*(label - prediction)
 
 if __name__ == "__main__":
-
-    #Entradas para treinamento
     Xj = []
-    Xj.append(np.array([0, 0]))
-    Xj.append(np.array([0, 1]))
-    Xj.append(np.array([1, 0]))
-    Xj.append(np.array([1, 1]))
-    print(Xj)
+    Xj.append(np.array([1,1]))
+    Xj.append(np.array([1,0]))
+    Xj.append(np.array([0,1]))
+    Xj.append(np.array([0,0]))
 
-    #Saídas esperadas
-    y = np.array([1, 0, 0, 1])
+    y = np.array([1,0,0,0])
 
-    #Neurônio artificial
-    na = perceptron(0.01, 50, 2)
+    and_perceptron = Perceptron(2,i=200, eta=0.1)
+    and_perceptron.train(Xj, y)
+
+    inputs = np.array([1,1])
+    print(and_perceptron.predict(inputs))
+
+
+    inputs = np.array([0,1])
+    print(and_perceptron.predict(inputs))
